2df6f7309fc53af96a1ac9da1e7a074d225f47a7
RakibRahman <md.rr.talukder@gmail.com>
2021-02-05 22:03:07 +0600

    Basic Setup Done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "sans-serif"],
+        mont: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
